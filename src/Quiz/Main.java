4abe5ac42075b1edd85c03341b71ac18afb7251f
Pasha <pavel-kargapolov@rambler.ru>
2018-10-08 02:04:26 +0600

    1

--- src/Quiz/Main.java
+++ src/Quiz/Main.java
@@ -2,7 +2,8 @@ package Quiz;
 import java.io.*;
 public class Main 
 {
-	public static void main(String[] args) throws IOException, FileException{		
+	public static void main(String[] args) throws IOException, FileException
+	{		
 		new Event().startGame();	
 	}
 }
