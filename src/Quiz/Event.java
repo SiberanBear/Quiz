8c4eba9b6d50895bf7fb0b212413d1d0866f8ee3
Pasha <pavel-kargapolov@rambler.ru>
2018-10-18 12:30:28 +0600

    add

--- src/Quiz/Event.java
+++ src/Quiz/Event.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Event 
+public class Event implements Runnable
 {	
 	private String selectTheme;
 	private String help = "Правила очень простые: Я буду задавать вопросы, "
@@ -20,7 +20,9 @@ public class Event
 	List<String> questList = new ArrayList<String>();
 	private int lastAns = 1;
 	private String lastQuest = null;
-	
+	boolean flag1 = true;
+	boolean flag2 = false;
+
 	public void gameQuiz(String name) throws FileException
 	{
 		int point = 0;
@@ -28,16 +30,19 @@ public class Event
 		String question = null;
 		while(true) {
 			if (choise == 0) {
-				System.out.println("YOU WIN!!!");
+				System.out.println(name + "YOU WIN!!!");
 				break;
 			}
-			question = selectQuest(question, choise);
+			question = selectQuest(choise, name);
 			int answer = enter.nextInt();
-			if (answer != dict.get(question) && answer != 0){	
+			if (answer != dict.get(question) && answer != 0 && answer != 5) {	
 				point -= 300;
-				System.out.println("Неверно! У тебя " + point + " очков");
+				System.out.println(name + "Неверно! У тебя " + point + " очков");
+				Collections.swap(questList, questList.indexOf(question), choise-1);
+				choise--;
 				if (point <= 0) {
 					System.out.println(name + ", GAME OVER!!!!");
+					flag1 = false;
 					break;
 				}
 			}
@@ -46,17 +51,27 @@ public class Event
 				this.lastQuest = question;
 				System.out.println(this.help);
 			}
+			else if (answer == 5) {
+				if (point <= 500)
+					System.out.println("У тебя недостаточно очков, чтобы взять подсказку");
+				else {			
+					getHint(question, dict.get(question));
+					point-=500;
+				}
+				this.lastAns = answer;
+				this.lastQuest = question;
+			}
 			else {
 				this.lastAns = answer;
 				point+=100;
-				System.out.println("Молодец! У тебя " + point + " очков");	
+				System.out.println(name + "Молодец! У тебя " + point + " очков");
+				Collections.swap(questList, questList.indexOf(question), choise-1);
+				choise--;
 			}
-			Collections.swap(questList, questList.indexOf(question), choise-1);
-			choise--;
 		}
 	}
 	
-	public void startGame() throws FileException
+	public  void startGame() throws FileException
 	{
 		System.out.println("Привет! Для начала скажи, как я могу к тебе обращаться?");		
 		Scanner enter = new Scanner(System.in);
@@ -64,10 +79,10 @@ public class Event
 		System.out.println("Приятно познакомиться, " + name + "!\nДавай сыграем! "
 				+ "\nДля ответа на вопросы используйте клавиши от 1 до 4. Справка вызывается по нажатию клавиши 0");
 		Question creater = new Question();
-		System.out.println("Для начала, выберите тематику вопросов: " + creater.dictTheme.get("1.txt") + " " + creater.dictTheme.get("2.txt"));	
+		System.out.println("Для начала, выберите тематику вопросов: " + creater.theme.get(0) + " " + creater.theme.get(1));	
 		this.selectTheme = enter.next();	
 		while (this.selectTheme.contains("0")) {			
-			System.out.println(this.help + "Тематики: " + creater.dictTheme.get("1.txt") + " " + creater.dictTheme.get("2.txt"));
+			System.out.println(this.help + "Тематики: " + creater.theme.get(0) + " " + creater.theme.get(1));
 			this.selectTheme = enter.next();			
 		}
 		creater.readFile(this.selectTheme);
@@ -76,14 +91,46 @@ public class Event
 		gameQuiz(name);
 	}
 	
-	public String selectQuest(String question, int choise) 
+	public String selectQuest(int choise, String name) 
 	{
+		String question = null;
 		if (this.lastAns == 0)
 			question = this.lastQuest;
+		else if (this.lastAns == 5) {
+			System.out.println(this.lastQuest);
+			return this.lastQuest;
+		}
 		else
 			question = questList.get(new Random().nextInt(choise));
 		String [] s = question.split(":");
-		System.out.println(s[0] + " \n" + s[1]);
+		System.out.println(name + s[0] + " \n" + s[1]);
 		return question;
+	}
+	
+	public void getHint(String question, int answer)
+	{
+		String [] s = question.split(":");
+		String[] s1 = s[1].split(";");
+		List<String> ans = new ArrayList<>();
+		List<String> notAns = new ArrayList<>();
+		for (String i : s1) {
+			if (i.contains(Integer.toString(answer)))
+				ans.add(i);
+			else
+				notAns.add(i);
+		}
+		ans.add(notAns.get(new Random().nextInt(3)));
+		System.out.println(s[0] + " " + ans.get(1) + " " + ans.get(0));
+		//this.lastQuest = s[0] + ans.get(1) + " " + ans.get(0);
+	}
+	@Override
+	public void run() {
+		try {
+			startGame();
+		} catch (FileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
