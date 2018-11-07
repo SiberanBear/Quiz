f9f3dc1be5c494500d0b471a15f372c930f010a2
Pasha <pavel-kargapolov@rambler.ru>
2018-11-07 22:01:24 +0600

    1

--- src/Quiz/FileException.java
+++ src/Quiz/FileException.java
@@ -3,10 +3,9 @@ package Quiz;
 public class FileException extends Exception{
 	public FileException(Throwable cause, String message) {
 		super(cause);
-		System.err.println("Данный файл не найден");
 		}
 	
-	public FileException(String message){
-		System.out.println(message);
+	public FileException(){
+		super();
 	}
 }
